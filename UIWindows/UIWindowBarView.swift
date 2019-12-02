e23932611aac05fa3ffbae8d494280c47582bdad
sinLuke <yinluke@me.com>
2019-12-02 11:41:58 -0400

    Release 2.0.0.

--- UIWindows/UIWindowBarView.swift
+++ UIWindows/UIWindowBarView.swift
@@ -9,37 +9,23 @@
 import UIKit
 
 class UIWindowBarView: UIView {
-    let gradient = CAGradientLayer()
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.backgroundColor = .clear
-        addGradient()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func addGradient(){
-        
-
-        gradient.frame = self.bounds
-        gradient.colors = [UIColor.init(displayP3Red: 226/255, green: 226/255, blue: 226/255, alpha: 1.0), UIColor.init(displayP3Red: 204/255, green: 204/255, blue: 204/255, alpha: 1.0)]
-
-        self.layer.insertSublayer(gradient, at: 0)
-    }
+    private let gradient : CAGradientLayer = CAGradientLayer()
+    private let gradientStartColor = UIColor.init(displayP3Red: 226/255, green: 226/255, blue: 226/255, alpha: 1.0)
+    private let gradientEndColor = UIColor.init(displayP3Red: 204/255, green: 204/255, blue: 204/255, alpha: 1.0)
     
     override func layoutSublayers(of layer: CALayer) {
         super.layoutSublayers(of: layer)
         gradient.frame = self.bounds
     }
 
+    override public func draw(_ rect: CGRect) {
+        gradient.frame = self.bounds
+        gradient.colors = [gradientEndColor.cgColor, gradientStartColor.cgColor]
+        gradient.startPoint = CGPoint(x: 1, y: 0)
+        gradient.endPoint = CGPoint(x: 0.2, y: 1)
+        if gradient.superlayer == nil {
+            layer.insertSublayer(gradient, at: 0)
+        }
+    }
 }
