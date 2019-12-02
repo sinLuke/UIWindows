008e6e0d2823ba4c42d6241f9012556119d30348
sinLuke <yinluke@me.com>
2019-12-02 13:55:42 -0400

    Release 2.0.0.

--- UIWindows/UIWindowsWindow.swift
+++ UIWindows/UIWindowsWindow.swift
@@ -175,7 +175,7 @@ public class UIWindowsWindow: UIView {
             
             if fullScreen {
                 print(self.safeAreaInsets.top)
-                fix(this: navigationVC.view, into: containerView, horizontal: .fill(leading: 0, trailing: 0), vertical: .fill(leading: self.config.barHeight + desktop?.view.safeAreaInsets.top ?? 0, trailing: 0))
+                fix(this: navigationVC.view, into: containerView, horizontal: .fill(leading: 0, trailing: 0), vertical: .fill(leading: self.config.barHeight + (desktop?.view?.safeAreaInsets.top ?? 0), trailing: 0))
             } else {
                 fix(this: navigationVC.view, into: containerView, horizontal: .fill(leading: self.config.windowEdgeWidth, trailing: self.config.windowEdgeWidth), vertical: .fill(leading: self.config.barHeight, trailing: self.config.windowEdgeWidth))
             }
